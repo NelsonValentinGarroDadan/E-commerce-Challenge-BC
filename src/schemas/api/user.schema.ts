a355328d419763cec17c70726eea5ed8e7164e08
Velentin Garro <nelsonvgarrodadan@gmail.com>
2025-04-15 19:43:50 -0300

    fixed/schema response user

--- src/schemas/api/user.schema.ts
+++ src/schemas/api/user.schema.ts
@@ -1,10 +1,11 @@
 import { z } from "zod";
 import { BaseResponseSchema } from "./baseResponse.schema";
+import { ProductSchema } from "./product.schema";
 
 export const UserReponseSchema = BaseResponseSchema(z.object(
   {
    name: z.string(),
    email: z.string(),
-   favorites: z.array(z.string()),
+   favorites: z.array(ProductSchema),
   })
 )
